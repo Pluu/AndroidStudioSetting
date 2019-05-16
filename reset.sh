faf9ca83a0036c86193c5ee76fe57a741492f219
hyeonseok.noh <hyeonseok.noh@navercorp.com>
2019-05-16 15:16:22 +0900

    Update Command

---
+++ reset.sh
@@ -0,0 +1,12 @@
+# https://qiita.com/nagaoyuriko/items/f310b595ff16061e1186
+
+echo "Reset, setting files..."
+
+# rm -Rf /Applications/Android\ Studio.app
+rm -Rf ~/Library/Preferences/AndroidStudio*
+# rm ~/Library/Preferences/com.google.android.studio.plist
+# rm -Rf ~/Library/Application\ Support/AndroidStudio*
+rm -Rf ~/Library/Logs/AndroidStudio*
+rm -Rf ~/Library/Caches/AndroidStudio*
+# rm -Rf ~/.gradle
+# rm -Rf ~/.android
