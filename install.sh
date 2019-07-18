cec4d9605705c62331de06e04d3f85ae68543fa8
pluulove <pluulove@gmail.com>
2019-07-18 23:46:18 +0900

    Update Script

--- install.sh
+++ install.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 # Installs IntelliJ templates
 
-echo "Installing template files..."
-
 # find /Applications -path "*.app" -prune -name "*Android Studio*" -print0 | while read -d $'\0' name
-find "/Users/$USER/Library/Preferences" -type d -name "*AndroidStudio*" -print0 | while read -d $'\0' folder
-do
-  echo "Installing to $folder"
-  cp -r templates "$folder"
-  # 
-done
+array=`find "/Users/$USER/Library/Preferences" -type d -name "*AndroidStudio*"`
 
+EXIT='exit'
+array=("$array $EXIT")
+folders=( "colors" "quicklists" "templates" )
+
+PS3="Please select : "
+
+select VAR in $array
+do
+    if [ $VAR = $EXIT ]; then
+        break
+    fi
+
+    echo ""
+    echo "Installing to $VAR"
+
+    # Install Folder Data
+    for folder in ${folders[*]}; do
+        echo ">>" $folder "<<"
+        cp -r $folder "$VAR"
+    done
+
+    # Install Files
+    cp disabled_plugins.txt "$VAR"
+
+    echo "Complete installed"
+    echo ""
+done
 echo "Done."
-echo ""
