deedfa776ff2197d10c4d67acf0bd2af6255c8fa
pluulove <pluulove@gmail.com>
2020-06-13 12:22:46 +0900

    Update Command and sample

--- install.sh
+++ install.sh
@@ -1,34 +1,91 @@
 #!/bin/bash
 # Installs IntelliJ templates
 
+# https://en.wikipedia.org/wiki/ANSI_escape_code
+#
+# ----------------------------------
+# Colors
+# ----------------------------------
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHTGRAY='\033[0;37m'
+DARKGRAY='\033[1;30m'
+LIGHTRED='\033[1;31m'
+LIGHTGREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LIGHTBLUE='\033[1;34m'
+LIGHTPURPLE='\033[1;35m'
+LIGHTCYAN='\033[1;36m'
+WHITE='\033[1;37m'
+
+## Print a horizontal rule
+divider () {
+	printf -v _hr "%*s" $(tput cols) && echo ${_hr// /${1--}}
+}
+
 # find /Applications -path "*.app" -prune -name "*Android Studio*" -print0 | while read -d $'\0' name
-array=`find "/Users/$USER/Library/Preferences" -type d -name "*AndroidStudio*"`
+
+# check, install root path
+DIR_LIST=(
+    "/Users/$USER/Library/Preferences"
+    "/Users/$USER/Library/ApplicationSupport/Google"
+)
+
+# find, installed path
+array=()
+for VAR in "${DIR_LIST[@]}"; 
+do
+    array+=(`find "$VAR" -type d -name "*AndroidStudio*"`)
+done
+
+# size check
+# echo "${#array[@]}"
 
 EXIT='exit'
-array=("$array $EXIT")
+array+=("$EXIT")
+
+# set, setting folders
 folders=( "colors" "quicklists" "templates" )
+files=( "disabled_plugins.txt" )
 
 PS3="Please select : "
 
-select VAR in $array
+# run select
+select VAR in "${array[@]}"; 
 do
     if [ $VAR = $EXIT ]; then
         break
     fi
 
     echo ""
-    echo "Installing to $VAR"
+    divider
+    echo "${LIGHTRED}Installing${NOCOLOR} to $VAR"
 
-    # Install Folder Data
-    for folder in ${folders[*]}; do
-        echo ">>" $folder "<<"
-        cp -r $folder "$VAR"
-    done
+    if (( ${#folders[@]} )); then
+        echo "${YELLOW}Folders${NOCOLOR}"
+        # Install Folder Data
+        for folder in ${folders[*]}; do
+            echo "  $folder"
+            cp -r $folder "$VAR"
+        done
+    fi
 
-    # Install Files
-    cp disabled_plugins.txt "$VAR"
+    if (( ${#files[@]} )); then
+        echo "${YELLOW}Files${NOCOLOR}"
+        # Install Files
+        for file in ${files[*]}; do
+            echo "  $file"
+            cp $file "$VAR"
+        done
+    fi
 
-    echo "Complete installed"
+    echo "${LIGHTCYAN}Complete${NOCOLOR} installed"
+    divider
     echo ""
 done
 echo "Done."
